d69c0c12721deb7fb0d7b75d78f5e9ab8c4c70ba
dastagg(opusmac) <dastagg@stagglinux.com>
2013-01-17 14:18:30 -0600

    Add Chapter 13 - Chat app.

---
+++ chpt13/README.md
@@ -0,0 +1,2 @@
+This is an update to the readme.
+
---
+++ chpt13/socket.io_express/app.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('welcome', { text: 'OH HAI! U R CONNECTED' });
+});
+
